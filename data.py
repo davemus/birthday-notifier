b559e1bb69b5cb1dea892117702ab9ed3a520bb9
davemus <drozdovud@gmail.com>
2021-10-11 02:55:38 +0300

    Make work google spreadsheet

--- data.py
+++ data.py
@@ -1,8 +1,51 @@
+import os
+from collections import namedtuple
+from datetime import date, datetime
+from itertools import chain
+from googleapiclient.discovery import build
+
+
+spreadsheet_id = os.environ['GOOGLE_SPREADSHEET_ID']
+birthdays_range = "'Дни рождения'!A2:B1000"
+emails_range = "'Получатели нотификаций'!A1:A1000"
+
+
+API_KEY = os.environ['GOOGLE_API_KEY']
+service = build('sheets', 'v4', developerKey=API_KEY)
+
+
+Person = namedtuple('Person', 'full_name birthday')
+
+
 def retrieve_email_addresses():
-    email = '64726f7a646f76756440676d61696c2e636f6d'
-    return [str(bytes.fromhex(email), encoding='utf-8')]
+    request = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id,
+        range=emails_range,
+    )
+    raw_response = request.execute()
+    return list(chain.from_iterable(raw_response.get('values', [])))
+
+
+def compare_days(date1, date2):
+    return date1.month == date2.month and date1.day == date2.day
 
 
 def retrieve_birthday_people():
-    return ['Кто-то Важный', 'Важный Кто-то']
+    request = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id,
+        range=birthdays_range,
+    )
+    raw_response = request.execute()
+    parse_date = lambda datestr: datetime.strptime(datestr, '%d.%m.%Y')
+    people = list(
+        map(
+            lambda x: Person(x[0], parse_date(x[1])),
+            raw_response.get('values', [])
+        )
+    )
+    today = date.today()
+    return [
+        person for person in people
+        if compare_days(person.birthday, today)
+    ]
 
--- main.py
+++ main.py
@@ -16,10 +16,13 @@ from emails import send_emails
 if __name__ == '__main__':
     try:
         if not emails_have_already_been_sent():
-            send_emails(
-                retrieve_email_addresses(),
-                retrieve_birthday_people(),
-            )
+            birthday_boys_and_girls = retrieve_birthday_people()
+            notified_people = retrieve_email_addresses()
+            if birthday_boys_and_girls and notified_people:
+                send_emails(
+                    notified_people,
+                    birthday_boys_and_girls,
+                )
             write_report_on_successful_sending()
     except Exception as e:
         write_report_on_exception(str(e))
